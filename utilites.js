32edb2719f176fa2ab08b9bd996a24f5f23935d8
Ahanaf Mubasshir <123632951+ahanaf607307@users.noreply.github.com>
2024-09-24 10:42:56 +0600

    updates some class and id for utilitis

--- utilites.js
+++ utilites.js
@@ -1,1 +1,32 @@
- 
+function getDonateBtnById(id){
+    const donateBtn = document.getElementById(id)
+    return donateBtn
+}
+
+function getInputFeildById(id){
+    const inputFeild = document.getElementById(id)
+    const inputFeildValue = inputFeild.value;
+    const inputFeildNumber = parseFloat(inputFeildValue)
+    return inputFeildNumber
+}
+
+function getBalanceById(id){
+    const mainBalance = document.getElementById(id)
+    const mainBalanceValue = mainBalance.innerText;
+    const mainBalanceValueNumber = parseFloat(mainBalanceValue)
+    return mainBalanceValueNumber;
+}
+
+function getMainBalanceById(id){
+    const mainBalanceTotal = document.getElementById(id)
+    const mainBalanceTotalValue = mainBalanceTotal.innerText;
+    const mainBalanceValueNumber = parseFloat(mainBalanceTotalValue)
+    return mainBalanceValueNumber
+}
+
+
+function getInputTextById(id){
+    const clearInputText = document.getElementById(id)
+
+    return clearInputText;
+}
